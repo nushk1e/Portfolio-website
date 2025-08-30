40d91bca98d748af72ab75d213135eb0242020f6
Anushka Srivastava <anushka.srivastava1502@gmail.com>
2025-08-30 22:45:00 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello Vue 3!</h1>
+    <p>Welcome to your new Vue 3 project.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
